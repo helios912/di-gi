52136ad818fc9d2e619eb32d50aac3c616aa392e
Mukola2 <janaon333@gmail.com>
2024-03-31 13:37:41 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
-
+$(function () {
+  $(".otzuvu__slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+  $(".top__slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 640,
+        settings: { arrows: false },
+      },
+    ],
+  });
 });
